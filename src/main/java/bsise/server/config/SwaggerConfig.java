6849f62cee2e10b5b44642f72b18c42a568690c7
Yeriimii <seojs0511@naver.com>
2024-10-02 17:17:26 +0900

    feat: swagger config 작성

---
+++ src/main/java/bsise/server/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package bsise.server.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("올려 올려 라디오 API")
+                .description("MBTI F, T 모두를 위로해주는 서비스의 API 문서입니다.")
+                .version("1.0");
+    }
+}
