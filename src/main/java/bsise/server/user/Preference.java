5c35168ee388b815b727fa6369ec12a1072f8759
Yeriimii <seojs0511@naver.com>
2024-10-02 17:45:47 +0900

    feat: 선호 유형 직렬화 기능 추가

--- src/main/java/bsise/server/user/Preference.java
+++ src/main/java/bsise/server/user/Preference.java
@@ -1,7 +1,19 @@
 package bsise.server.user;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
+
 public enum Preference {
 
     F,
-    T
+    T,
+    ;
+
+    @JsonCreator
+    public static Preference of(String preference) {
+        return Arrays.stream(values())
+                .filter(pref -> pref == valueOf(preference.toUpperCase()))
+                .findAny()
+                .orElse(F);
+    }
 }
