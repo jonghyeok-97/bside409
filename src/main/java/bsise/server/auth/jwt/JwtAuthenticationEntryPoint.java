6a01f31c962ab2ffba9912877197e6e78bf10e3b
Yeriimii <seojs0511@naver.com>
2024-10-04 18:54:19 +0900

    fix: jwtEntryPoint 응답 변경

--- src/main/java/bsise/server/auth/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/bsise/server/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -1,15 +1,11 @@
 package bsise.server.auth.jwt;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import feign.Response;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -18,21 +14,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final ObjectMapper objectMapper;
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        sendErrorResponse(response, "인증에 실패했습니다");
-    }
-
-    private void sendErrorResponse(HttpServletResponse response, String message) throws IOException {
-        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.getWriter().write(objectMapper.writeValueAsString(Response.builder()
-                .status(HttpStatus.FORBIDDEN.value())
-                .body(message, StandardCharsets.UTF_8)
-                .build()));
+        response.sendError(HttpStatus.UNAUTHORIZED.value(), "인증에 실패했습니다");
     }
 }
