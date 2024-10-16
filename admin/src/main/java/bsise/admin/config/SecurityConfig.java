da6f15ec87cd992560351c8f4b5687d2787eac19
jellyyelly <seojs0511@naver.com>
2024-10-17 03:46:17 +0900

    refactor: CORS 설정 분리

--- admin/src/main/java/bsise/admin/config/SecurityConfig.java
+++ admin/src/main/java/bsise/admin/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package bsise.admin.config;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -10,9 +11,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+
+    @Value("${base-url}")
+    private String baseUrl;
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -20,20 +26,39 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable);
 
         // cors
-        http.cors(config -> config.configurationSource(request -> {
-            CorsConfiguration corsConfig = new CorsConfiguration();
-            corsConfig.setAllowedOrigins(Collections.singletonList("localhost:5173/login"));
-            corsConfig.setAllowedMethods(Arrays.asList("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
-            corsConfig.setAllowedHeaders(Collections.singletonList("*"));
-            corsConfig.setAllowCredentials(true);
-            corsConfig.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION));
-            corsConfig.setMaxAge(3600L);
-            return corsConfig;
-        }));
+        http.cors(config -> config.configurationSource(corsConfigurationSource()));
 
         // url pattern
-        http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll()); // FIXME: 개발 후 `.authenticated()`
-        http.formLogin(form -> form.loginPage("localhost:5173/login"));
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/login").permitAll()
+                .anyRequest().authenticated());
+
+        // login
+        http.formLogin(form -> form.loginPage(baseUrl + "/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl(baseUrl + "/")
+        );
+
+        // logout
+        http.logout(form -> form.logoutUrl("/logout")
+                .logoutSuccessUrl(baseUrl + "/login")
+                .deleteCookies("JSESSIONID")
+        );
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList(baseUrl));
+        corsConfig.setAllowedMethods(Arrays.asList("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
+        corsConfig.setAllowedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setExposedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+        corsConfig.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
     }
 }
