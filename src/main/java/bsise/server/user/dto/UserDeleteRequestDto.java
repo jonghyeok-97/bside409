595e164586092a1fa660ec4115b590a861620cb5
jellyyelly <seojs0511@naver.com>
2024-10-25 01:46:33 +0900

    chore: delete request dto schema 수정

--- src/main/java/bsise/server/user/dto/UserDeleteRequestDto.java
+++ src/main/java/bsise/server/user/dto/UserDeleteRequestDto.java
@@ -4,21 +4,25 @@ import bsise.server.user.domain.WithdrawalReason;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.util.StringUtils;
 
+@Tag(name = "user")
 @Schema(description = "회원 탈퇴 요청 DTO")
 @Getter
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDeleteRequestDto {
 
-    @Schema(description = "유저가 선택한 번호", exampleClasses = WithdrawalReason.class, examples = {"selectedNumber: 1 => 자주 사용하지 않아요."})
+    @Schema(description = "유저가 선택한 번호. "
+            + "0: 기타, 1: 자주 사용하지 않아요, 2: 개인 정보가 걱정돼요, 3: 사용하기가 불편해요, 4:답장이 마음에 들지 않아요",
+            allowableValues = {"0", "1", "2", "3", "4"},
+            example = "selectedNumber: 1 => '자주 사용하지 않아요.' 로 처리")
+    @Range(min = 0, max = 4)
     private int selectedNumber;
 
     @Schema(description = "유저가 입력한 상세 탈퇴 사유. 입력이 없으면 해당 필드를 포함시키지 않는다.", requiredMode = RequiredMode.NOT_REQUIRED)
