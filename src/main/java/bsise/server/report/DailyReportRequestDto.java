3aa5970948186799a5c02b7f654d265d10d98a8c
곽태영 <amberaplin14@gmail.com>
2024-11-18 00:20:25 +0900

    fix: LocalDate NotNull 어노테이션으로 검증하도록 수정
    - 기존에는 문자열 타입에만 사용 가능한 NotBlank 어노테이션 사용

--- src/main/java/bsise/server/report/DailyReportRequestDto.java
+++ src/main/java/bsise/server/report/DailyReportRequestDto.java
@@ -2,6 +2,7 @@ package bsise.server.report;
 
 import bsise.server.validation.constraints.WithinLastMonth;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -14,7 +15,7 @@ public class DailyReportRequestDto {
     @NotBlank(message = "유저의 아이디가 존재하지 않습니다.")
     private final String userId;
 
-    @NotBlank(message = "생성 요청 날짜는 필수 요청 값입니다.")
+    @NotNull(message = "생성 요청 날짜는 필수 요청 값입니다.")
     @WithinLastMonth(message = "생성 요청 날짜는 오늘 포함 한달 이전까지만 가능합니다.")
     private final LocalDate date;
 }
