71793289c2701b1a3811f54cd876a74c098e26ef
곽태영 <amberaplin14@gmail.com>
2024-11-18 06:01:26 +0900

    feat: Clova 일일분석 응답메시지 매핑 Dto 추가

---
+++ src/main/java/bsise/server/clovar/AnalysisResult.java
@@ -0,0 +1,36 @@
+package bsise.server.clovar;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AnalysisResult {
+
+    @JsonProperty(value = "letterAnalyses")
+    private List<LetterAnalysis> letterAnalyses;
+
+    @JsonProperty(value = "dailyCoreEmotion")
+    private String dailyCoreEmotion;
+
+    @JsonProperty(value = "description")
+    private String description;
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class LetterAnalysis {
+        @JsonProperty(value = "letterIndex")
+        private int letterIndex;
+
+        @JsonProperty(value = "coreEmotions")
+        private List<String> coreEmotions;
+
+        @JsonProperty(value = "sensitiveEmotions")
+        private List<String> sensitiveEmotions;
+
+        @JsonProperty(value = "topic")
+        private String topic;
+    }
+}
