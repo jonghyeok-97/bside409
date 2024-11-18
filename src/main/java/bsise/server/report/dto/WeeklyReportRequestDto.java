960d75c275f5eafcd785efd437faaa715e6f121e
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-11-18 14:15:50 +0900

    refactor: 주간 분석 요청 DTO의 필드를 LocalDate 로 변경
    기획 요구 사항에 따른 변경

--- src/main/java/bsise/server/report/dto/WeeklyReportRequestDto.java
+++ src/main/java/bsise/server/report/dto/WeeklyReportRequestDto.java
@@ -3,10 +3,11 @@ package bsise.server.report.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.constraints.Range;
 
 @Schema(description = "주간 분석 요청 DTO")
 @Getter
@@ -17,10 +18,8 @@ public class WeeklyReportRequestDto {
     @NotBlank(message = "유저의 아이디가 존재하지 않습니다.")
     private final UUID userId;
 
-    @Schema(description = "유저가 주간 분석을 받을 주차",
-            minimum = "1",
-            maximum = "53",
+    @Schema(description = "유저가 주간 분석을 받을 주차의 시작 날짜",
             requiredMode = RequiredMode.REQUIRED)
-    @Range(min = 1, max = 53, message = "주간 분석을 받을 주차를 선택해 주세요.")
-    private final int week;
+    @NotNull(message = "주간 분석을 받을 주차의 시작 날짜를 입력해 주세요.")
+    private final LocalDate startDate;
 }
