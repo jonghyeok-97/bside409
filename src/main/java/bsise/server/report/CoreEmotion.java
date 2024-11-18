258db2842729b5c4243c008cdb569e61571b2112
곽태영 <amberaplin14@gmail.com>
2024-11-18 14:20:09 +0900

    fix: 슬픔 감정 오타 수정

--- src/main/java/bsise/server/report/CoreEmotion.java
+++ src/main/java/bsise/server/report/CoreEmotion.java
@@ -3,7 +3,7 @@ package bsise.server.report;
 import java.util.Arrays;
 
 public enum CoreEmotion {
-    기쁨, 슬쁨, 분노, 두려움, 놀라움, 혐오, 열망, 수용, 중립;
+    기쁨, 슬픔, 분노, 두려움, 놀라움, 혐오, 열망, 수용, 중립;
 
     public static boolean isAbsent(String emotion) {
         return Arrays.stream(values())
