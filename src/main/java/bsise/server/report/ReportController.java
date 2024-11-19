a632fbe216f59e950c5bb20e7b54a4ddd5005b10
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-11-19 10:47:17 +0900

    feat: 주간 분석 컨트롤러 추가

--- src/main/java/bsise/server/report/ReportController.java
+++ src/main/java/bsise/server/report/ReportController.java
@@ -1,13 +1,20 @@
 package bsise.server.report;
 
+import bsise.server.report.weekly.dto.WeeklyReportRequestDto;
+import bsise.server.report.weekly.dto.WeeklyReportResponseDto;
 import jakarta.validation.Valid;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/v1/reports", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,5 +34,11 @@ public class ReportController {
     public DailyReportResponseDto getDailyReport(
             @RequestParam LocalDate date, @Validated(DailyReportGetRequest.class) @RequestBody DailyReportRequestDto dailyReportDto) {
         return reportService.getDailyReport(dailyReportDto.getUserId(), date);
+    }
+
+    @PostMapping("/weekly")
+    @ResponseStatus(HttpStatus.OK)
+    public WeeklyReportResponseDto createWeeklyReport(@Valid @RequestBody WeeklyReportRequestDto weeklyReportRequestDto) {
+        return reportService.createWeeklyReport(weeklyReportRequestDto);
     }
 }
