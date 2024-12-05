5c27888ce09b7ab78c95617d16559f0b9243b87a
jellyyelly <seojs0511@naver.com>
2024-12-05 12:50:39 +0900

    chore: 리포트 상태 조회 컨트롤러에 프로필 적용

--- src/main/java/bsise/server/report/retrieve/controller/DevReportStatusRetrieveController.java
+++ src/main/java/bsise/server/report/retrieve/controller/DevReportStatusRetrieveController.java
@@ -13,31 +13,32 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile({"prod"})
+@Profile({"dev", "test"})
 @RestController
 @RequiredArgsConstructor
-public class ReportStatusRetrieveController {
+public class DevReportStatusRetrieveController {
 
     private final ReportStatusRetrieveService reportStatusRetrieveService;
 
     @GetMapping(value = "/api/v1/reports/daily/status")
     @ResponseStatus(HttpStatus.OK)
     public List<DailyReportStatusResponseDto> retrieveDailyReportStatus(
+            @RequestParam("date") LocalDate date,
             @Valid @RequestBody ReportStatusRequestDto requestDto
     ) {
-        LocalDate now = LocalDate.now();
-        return reportStatusRetrieveService.findDailyReportStatus(UUID.fromString(requestDto.getUserId()), now, now);
+        return reportStatusRetrieveService.findDailyReportStatus(UUID.fromString(requestDto.getUserId()), date, date);
     }
 
     @GetMapping(value = "/api/v1/reports/weekly/status")
     @ResponseStatus(HttpStatus.OK)
     public List<WeeklyReportStatusResponseDto> retrieveWeeklyReportStatus(
+            @RequestParam("date") LocalDate date,
             @Valid @RequestBody ReportStatusRequestDto requestDto
     ) {
-        LocalDate now = LocalDate.now();
-        return reportStatusRetrieveService.findWeeklyReportStatus(UUID.fromString(requestDto.getUserId()), now, now);
+        return reportStatusRetrieveService.findWeeklyReportStatus(UUID.fromString(requestDto.getUserId()), date, date);
     }
 }
