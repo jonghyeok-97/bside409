2a380775f54daabc9e842460aa14b5518d0fed7f
jellyyelly <seojs0511@naver.com>
2024-12-13 20:21:55 +0900

    refactor: Custom Converter 로 변경

--- src/main/java/bsise/server/config/WebConfig.java
+++ src/main/java/bsise/server/config/WebConfig.java
@@ -1,8 +1,7 @@
 package bsise.server.config;
 
-import java.time.YearMonth;
+import bsise.server.common.converter.StringToYearMonthConverter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,12 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter((Converter<String, YearMonth>) source -> {
-            try {
-                return YearMonth.parse(source);
-            } catch (Exception e) {
-                throw new IllegalArgumentException("올바른 형식이 아닙니다. 'yyyy-MM' 형식이어야 합니다." + source);
-            }
-        });
+        registry.addConverter(new StringToYearMonthConverter());
     }
 }
