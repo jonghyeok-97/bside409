2d86b2a50c8af7f96689a8fa58daa5197f0863f8
jellyyelly <seojs0511@naver.com>
2024-12-20 11:54:37 +0900

    fix: 애너테이션 수정

--- src/main/java/bsise/server/limiter/RateLimitController.java
+++ src/main/java/bsise/server/limiter/RateLimitController.java
@@ -2,9 +2,11 @@ package bsise.server.limiter;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/policies")
+@RestController
+@RequestMapping("/api/v1/policies")
 @RequiredArgsConstructor
 public class RateLimitController {
 
