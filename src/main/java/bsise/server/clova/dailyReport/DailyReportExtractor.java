7c01bbac0a173a3adf6bc2f57824567ab7050204
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-12-20 11:01:37 +0900

    refactor: 프롬프트에 CoreEmotion에 해당하지 않는 응답이 올 경우 '중립' 으로 변경
    CoreEmotion 에 해당하지 않는 감정들은 다 중립이니,
    1. DailyReportExtractor에 있는 필터링 부분은 없어져도 될거 같고
    2. 일일 분석 생성 시 프롬프트 응답으로 온 String -> CoreEmotion 로 변경할 때, valueOf가 아닌 CoreEmotion에 해당하지 않는 감정들은 중립으로 되도록 변경
    3. 의견에 동의하시면 DailyReportExtractor 에서 주석처리한 부분 지우고 다시 커밋 올리겠습니다.

--- src/main/java/bsise/server/clova/dailyReport/DailyReportExtractor.java
+++ src/main/java/bsise/server/clova/dailyReport/DailyReportExtractor.java
@@ -14,22 +14,22 @@ public class DailyReportExtractor {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
-            ClovaDailyAnalysisResult clovaDailyAnalysisResult = objectMapper.readValue(response.getResultMessage(), ClovaDailyAnalysisResult.class);
+            return objectMapper.readValue(response.getResultMessage(), ClovaDailyAnalysisResult.class);
 
             // 감정 목록에 없는 감정 필터링
-            Set<String> invalidEmotions = clovaDailyAnalysisResult.getLetterAnalyses().stream()
-                    .flatMap(analysis -> analysis.getCoreEmotions().stream())
-                    .filter(CoreEmotion::isAbsent)
-                    .collect(Collectors.toSet());
+//            Set<CoreEmotion> coreEmotions = clovaDailyAnalysisResult.getLetterAnalyses().stream()
+//                    .flatMap(analysis -> analysis.getCoreEmotions().stream())
+//                    .map(CoreEmotion::findOrNeutral)
+//                    .collect(Collectors.toSet());
+//
+//            // 일일 대표 감정 검증
+//            CoreEmotion coreEmotion = CoreEmotion.findOrNeutral(clovaDailyAnalysisResult.getDailyCoreEmotion());
+//
+//            if (coreEmotions.isEmpty()) {
+//                throw new IllegalArgumentException("대표 감정 목록에 존재하지 않는 감정이 분석되었습니다");
+//            }
 
-            // 일일 대표 감정 검증
-            boolean dailyCoreEmotionInvalid = CoreEmotion.isAbsent(clovaDailyAnalysisResult.getDailyCoreEmotion());
-
-            if (!invalidEmotions.isEmpty() || dailyCoreEmotionInvalid) {
-                throw new IllegalArgumentException("대표 감정 목록에 존재하지 않는 감정이 분석되었습니다: " + invalidEmotions);
-            }
-
-            return clovaDailyAnalysisResult;
+//            return clovaDailyAnalysisResult;
         } catch (JsonProcessingException e) {
             throw new IllegalArgumentException("답변 형식이 잘못되었습니다. 답변받은 데이터: " + response.getResultMessage(), e);
         }
