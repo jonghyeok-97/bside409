4069f8825119a517950bab38b3003e8ec69d1a01
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-12-22 20:56:03 +0900

    feat: clovaFeign 에 400,500번대 오류 발생 시 ErrorDecoder를 활용해 ERROR레벨의 로그 추가
    ErrorDecoder가 없으면 400,500번대 오류 발생 시 로그가 없기 때문

--- src/main/java/bsise/server/clova/client/ClovaFeignConfig.java
+++ src/main/java/bsise/server/clova/client/ClovaFeignConfig.java
@@ -5,6 +5,7 @@ import static feign.Logger.Level.HEADERS;
 import feign.Logger;
 import feign.Request;
 import feign.Response;
+import feign.codec.ErrorDecoder;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,11 @@ public class ClovaFeignConfig {
     @Bean
     public CustomFeignLogging customFeignLogging() {
         return new CustomFeignLogging();
+    }
+
+    @Bean
+    public CustomFeignErrorDecoder customFeignErrorDecoder() {
+        return new CustomFeignErrorDecoder();
     }
 
     @Slf4j
@@ -75,6 +81,37 @@ public class ClovaFeignConfig {
 
         protected String format(String configKey, String format, Object... args) {
             return String.format(methodTag(configKey) + format, args);
+        }
+    }
+
+    @Slf4j
+    static class CustomFeignErrorDecoder implements ErrorDecoder {
+
+        @Override
+        public Exception decode(String methodKey, Response response) {
+            int statusCode = response.status();
+            String stringBody;
+            try {
+                byte[] byteArray = getResponseBodyByteArray(response);
+                stringBody = new String(byteArray, StandardCharsets.UTF_8);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (400 <= statusCode && statusCode < 500) {
+                log.error("[Status: {}] UpUpRadio 서버 --> 클로바 서버 호출 실패 [Body: {}]", statusCode, stringBody);
+            }
+            if (500 <= statusCode) {
+                log.error("[Status: {}] 클로바 서버 오류 [Body: {}]", statusCode, stringBody);
+            }
+            throw new RuntimeException();
+        }
+
+        private byte[] getResponseBodyByteArray(Response response) throws IOException {
+            if (response.body() == null) {
+                return new byte[]{};
+            }
+            return StreamUtils.copyToByteArray(response.body().asInputStream());
         }
     }
 }
