439d5e1412d6ec0a15ae28623374527dbdab6eb7
jellyyelly <seojs0511@naver.com>
2024-12-29 04:28:58 +0900

    feat: 내 편지함 조회 시 published 값에 따라 동적 쿼리 수행

--- src/main/java/bsise/server/reply/ReplyRepository.java
+++ src/main/java/bsise/server/reply/ReplyRepository.java
@@ -1,18 +1,16 @@
 package bsise.server.reply;
 
 import bsise.server.letter.Letter;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReplyRepository extends JpaRepository<Reply, UUID> {
+public interface ReplyRepository extends JpaRepository<Reply, UUID>, ReplyQuerydslRepository {
 
     Optional<Reply> findByLetter(Letter letter);
 
@@ -25,14 +23,4 @@ public interface ReplyRepository extends JpaRepository<Reply, UUID> {
     @Deprecated
     @Query("SELECT r FROM Reply r where r.letter.user.id = :userId ORDER BY r.createdAt DESC")
     List<Reply> findTopNRepliesByUserId(UUID userId, Pageable pageable);
-
-    @Query("""
-            SELECT r
-            FROM Reply r
-            WHERE r.letter.user.id = :userId
-                AND r.createdAt >= :startOfYear AND r.createdAt <= :endOfYear AND r.letter.published = :published
-            ORDER BY r.createdAt DESC
-            """)
-    Page<Reply> findRepliesByOrderByCreatedAt(UUID userId, LocalDateTime startOfYear, LocalDateTime endOfYear,
-                                              boolean published, Pageable pageable);
 }
