c9deb8beae3e8030be918d1a6397a5699d62111f
jellyyelly <seojs0511@naver.com>
2024-12-29 18:29:11 +0900

    chore: CORS 설정을 환경 변수에서 관리하도록 변경

--- src/main/java/bsise/server/config/SecurityConfigDev.java
+++ src/main/java/bsise/server/config/SecurityConfigDev.java
@@ -12,6 +12,7 @@ import bsise.server.auth.jwt.JwtService;
 import java.util.Arrays;
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -30,6 +31,8 @@ import org.springframework.web.cors.CorsConfiguration;
 @Profile("dev")
 public class SecurityConfigDev {
 
+    @Value("${security.base-url}")
+    private String baseUrl;
     private final JwtService jwtService;
     private final UpOAuth2UserService upOAuth2UserService;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
@@ -56,7 +59,7 @@ public class SecurityConfigDev {
 
         // oauth2 (dev)
         http.oauth2Login(oauth2 -> oauth2
-                        .loginPage("http://localhost:3000" + "/login")
+                        .loginPage(baseUrl + "/login")
                         .userInfoEndpoint(config -> config.userService(upOAuth2UserService))
                         .successHandler(oAuth2SuccessHandler))
                 .logout(LogoutConfigurer::permitAll)
@@ -73,7 +76,7 @@ public class SecurityConfigDev {
     @Bean
     public CorsConfiguration corsConfiguration() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        corsConfig.setAllowedOrigins(Collections.singletonList(baseUrl));
         corsConfig.setAllowedMethods(Arrays.asList("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
         corsConfig.setAllowedHeaders(Collections.singletonList("*"));
         corsConfig.setAllowCredentials(true);
