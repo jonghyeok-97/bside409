70c41f3e06b1e01229c4a00e8bda834e7a31b5f8
sharpie1330 <amberaplin14@gmail.com>
2025-01-05 00:16:21 +0900

    refactor: Letter 생성 시 Preference 검증 로직 수정

--- src/main/java/bsise/server/letter/LetterRequestDto.java
+++ src/main/java/bsise/server/letter/LetterRequestDto.java
@@ -4,7 +4,7 @@ import bsise.server.user.domain.Preference;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -22,8 +22,9 @@ public class LetterRequestDto {
     private final String message;
 
     @Schema(description = "유저가 선호하는 답변 유형", allowableValues = {"F", "T"}, requiredMode = RequiredMode.REQUIRED)
-    @NotNull(message = "선호하는 답변 유형이 없습니다.")
-    private final Preference preference;
+    @NotBlank(message = "선호하는 답변 유형이 없습니다.")
+    @Pattern(regexp = "F|T", message = "선호하는 답변 유형은 F나 T 중 하나여야 합니다.")
+    private final String preference;
 
     @Schema(description = "편지 공개 여부", allowableValues = {"true", "false"}, requiredMode = RequiredMode.REQUIRED)
     private final boolean published;
@@ -31,7 +32,7 @@ public class LetterRequestDto {
     public Letter toLetterWithoutUser() {
         return Letter.builder()
                 .message(message)
-                .preference(preference)
+                .preference(Preference.of(preference))
                 .published(published)
                 .build();
     }
