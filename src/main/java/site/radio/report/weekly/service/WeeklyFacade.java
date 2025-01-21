a1518cf8a44887f4472ace3bf173c668d3149404
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-01-21 21:42:36 +0900

    refactor: 주간Facade계층에서 일일 리포트 생성할 때, 비동기로 요청하는 API를 사용하도록 변경

--- src/main/java/site/radio/report/weekly/service/WeeklyFacade.java
+++ src/main/java/site/radio/report/weekly/service/WeeklyFacade.java
@@ -3,7 +3,9 @@ package site.radio.report.weekly.service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ import site.radio.clova.dailyReport.DailyReportMessageParser;
 import site.radio.clova.dto.ClovaResponseDto;
 import site.radio.clova.service.ClovaService;
 import site.radio.clova.weekly.ClovaWeeklyReportRequestDto;
+import site.radio.error.ClovaErrorException;
 import site.radio.letter.Letter;
 import site.radio.letter.LetterService;
 import site.radio.report.daily.domain.DailyReport;
@@ -34,33 +37,53 @@ public class WeeklyFacade {
         // 주간 리포트 생성 여부 확인
         weeklyReportService.vaildateWeeklyReportBy(userId, startDate);
 
-        // 클로바에게 요청을 보낼 사용자의 1주일치 편지들 찾기
+        // 클로바에게 요청을 보낼 1주일치 편지 조회
         Map<LocalDate, List<Letter>> lettersForDailyReport = letterService.findLettersForDailyReport(
                 userId, startDate, startDate.plusDays(6));
 
-        // 일일 리포트의 편지들을 가공하고, 클로바에게 요청
-        Map<ClovaDailyAnalysisResult, List<Letter>> lettersByAnalysisResult = lettersForDailyReport.values().stream()
-                .collect(Collectors.toMap(
-                        letters -> {
-                            ClovaResponseDto clovaResponseDto = clovaService.sendDailyReportRequest(letters);
-                            return DailyReportMessageParser.extract(clovaResponseDto);
-                        },
-                        letters -> letters
-                ));
+        // 일일 리포트를 만들 편지들을 Clova에게 전송
+        Map<CompletableFuture<ClovaDailyAnalysisResult>, List<Letter>> futuresToLetters = sendAsyncLettersForDailyReport(lettersForDailyReport);
+
+        // 비동기 결과 기다리기
+        Map<ClovaDailyAnalysisResult, List<Letter>> analysisResultToLetters = resolveDailyReportFutures(futuresToLetters);
 
         // 클로바로부터 받은 일일 리포트의 분석결과들을 저장
-        dailyReportService.saveClovaDailyAnalysisResult(lettersByAnalysisResult);
+        dailyReportService.saveClovaDailyAnalysisResult(analysisResultToLetters);
 
         // 주간 분석에 필요한 위로 한마디 구하기
         List<LocalDate> oneWeek = createOneWeek(startDate);
         List<DailyReport> dailyReports = dailyReportService.getDailyReportsOfOneWeek(userId, oneWeek);
-        ClovaWeeklyReportRequestDto clovaWeeklyReportRequestDto = ClovaWeeklyReportRequestDto.from(combineDescription(dailyReports));
+        ClovaWeeklyReportRequestDto clovaWeeklyReportRequestDto = ClovaWeeklyReportRequestDto.from(
+                combineDescription(dailyReports));
 
         // 주간 분석 요청 및 응답
         ClovaResponseDto clovaResponseDto = clovaService.sendWeeklyReportRequest(clovaWeeklyReportRequestDto);
         String resultMessage = clovaResponseDto.getResultMessage();
 
         return weeklyReportService.createWeeklyReport(userId, oneWeek, resultMessage);
+    }
+
+    private Map<ClovaDailyAnalysisResult, List<Letter>> resolveDailyReportFutures(
+            Map<CompletableFuture<ClovaDailyAnalysisResult>, List<Letter>> lettersByFutures) {
+        return lettersByFutures.entrySet().stream()
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey().join(),
+                        Entry::getValue
+                ));
+    }
+
+    private Map<CompletableFuture<ClovaDailyAnalysisResult>, List<Letter>> sendAsyncLettersForDailyReport(
+            Map<LocalDate, List<Letter>> lettersForDailyReport) {
+        return lettersForDailyReport.values()
+                .stream()
+                .collect(Collectors.toMap(
+                        letters -> clovaService.sendAsyncDailyReportRequest(letters)
+                                .thenApply(DailyReportMessageParser::extract)
+                                .exceptionally(ex -> {
+                                    throw new ClovaErrorException("Clova 비동기 중 오류 발생" + Thread.currentThread().getName());
+                                }),
+                        letters -> letters
+                ));
     }
 
     private List<LocalDate> createOneWeek(LocalDate startDate) {
