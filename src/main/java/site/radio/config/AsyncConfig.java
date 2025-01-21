4deae40d410b9f29e5adbb4bc2bf5aff615a0be4
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-01-21 21:38:52 +0900

    feat: 비동기 쓰레드 풀 설정 추가

---
+++ src/main/java/site/radio/config/AsyncConfig.java
@@ -0,0 +1,28 @@
+package site.radio.config;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public Executor clovaExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(16);
+        executor.setMaxPoolSize(32);
+        executor.setQueueCapacity(100);
+        executor.setKeepAliveSeconds(60); // 기본 값 60
+        executor.setThreadNamePrefix("clove-async-");
+        executor.setAwaitTerminationSeconds(60);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setRejectedExecutionHandler(new CallerRunsPolicy());
+        executor.initialize();
+        return executor;
+    }
+}
